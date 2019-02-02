eee7071c6e524bc2ff33ce2e4fccb7e982903b4c
Jessy Hu <jesshuy12@gmail.com>
2019-02-02 06:14:52 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0 
   selected = []
     while i < collection.length 
-    selected.push(collection[i] if yield)
+    selected.push(collection[i] if yield(collection[i]))
  end
  return selected
 end
